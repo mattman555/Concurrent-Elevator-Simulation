a0f38c5d39259d27ac3bfb906193d0d3bc14c225
Nick Coutts <Nickcoutts99@gmail.com>
2021-02-06 17:34:25 -0500

    added tests for the Request class and wrote docs for the request class

--- SYSC3303Project/src/elevatorSystems/Request.java
+++ SYSC3303Project/src/elevatorSystems/Request.java
@@ -14,12 +14,19 @@ public class Request {
 	private Direction floorButton;
 	private int carButton;
 	
+	/**
+	 * Constructor for a new Request
+	 * @param time A string representation of a time in the format hh:mm:ss.mmm
+	 * @param floor the initial floor that a request comes form
+	 * @param floorButton the Direction the request wants to go
+	 * @param carButton the requested destination
+	 */
 	public Request(String time, int floor, String floorButton, int carButton) {
 		this.time = new int[4];
 		String[] timeArr = time.split(":");
 		this.time[0] = Integer.parseInt(timeArr[0]);
 		this.time[1] = Integer.parseInt(timeArr[1]);
-		timeArr = timeArr[2].split("\\.");
+		timeArr = timeArr[2].split("\\."); //split on periods
 		this.time[2] = Integer.parseInt(timeArr[0]);
 		this.time[3] = Integer.parseInt(timeArr[1]);
 		this.floor = floor;
@@ -28,6 +35,7 @@ public class Request {
 	}
 
 	/**
+	 * Return an array where the first element is the hours, second element is the minutes, third element is seconds, and the fourth element is the milliseconds
 	 * @return the time
 	 */
 	public int[] getTime() {
@@ -35,29 +43,35 @@ public class Request {
 	}
 
 	/**
-	 * @return the floor
+	 * returns the floor the request was initiated from
+	 * @return the floor the request was initiated from
 	 */
 	public int getFloor() {
 		return floor;
 	}
 
 	/**
-	 * @return the floorButtons
+	 * Returns the Direction the request wants to go
+	 * @return the floorButton representing the Direction the request is going
 	 */
 	public Direction getFloorButton() {
 		return floorButton;
 	}
 
 	/**
-	 * @return the carButton
+	 * Gets the destination of the current request
+	 * @return the carButton which is the destination of the request
 	 */
 	public int getCarButton() {
 		return carButton;
 	}
 	
+	/**
+	 * Returns a string representation of the request
+	 */
 	public String toString() {
 		String time = Integer.toString(this.time[0]) + ":" + Integer.toString(this.time[1]) + ":"  + Integer.toString(this.time[2]) + ":" + Integer.toString(this.time[3]);
-		return ("Request{Time: " + time + "| Floor: " + this.floor + "| Directional button: " + this.floorButton + "| Destination: " + this.carButton + "}");
+		return ("Request{Time: " + time + " | Floor: " + this.floor + " | Directional button: " + this.floorButton + " | Destination: " + this.carButton + "}");
 		
 	}
 
