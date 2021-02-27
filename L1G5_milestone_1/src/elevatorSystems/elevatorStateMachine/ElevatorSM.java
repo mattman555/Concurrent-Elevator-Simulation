a87e7665021775a07a6dbf2dd14e030c588dc55e
Nick Coutts <Nickcoutts99@gmail.com>
2021-02-27 13:07:21 -0500

    Removed unused lines

--- L1G5_milestone_1/src/elevatorSystems/elevatorStateMachine/ElevatorSM.java
+++ L1G5_milestone_1/src/elevatorSystems/elevatorStateMachine/ElevatorSM.java
@@ -8,8 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
-import javax.print.attribute.standard.Destination;
-
 import elevatorSystems.Direction;
 import elevatorSystems.Elevator;
 import elevatorSystems.FloorSubsystem;
@@ -20,97 +18,82 @@ import elevatorSystems.FloorSubsystem;
  */
 public class ElevatorSM implements Runnable{
 
-	private ElevatorState[] states;
-	private ElevatorStates curr = ElevatorStates.DOORS_CLOSED;
-	private HashMap<ElevatorStates, List<ElevatorStates>> transition;
-	private int current;
-	private int[][] transitions = {{2,1,5},{2},{3},{4},{0}};
+	private ElevatorStates current;
+	HashMap<ElevatorStates, ElevatorState> states;
+	private HashMap<ElevatorStates, List<ElevatorStates>> transitions;
 	private Elevator elevator;
 	private FloorSubsystem floorSubsystem;
-	HashMap<ElevatorStates, ElevatorState> s;
+;
 	/**
 	* 
 	*/
 	public ElevatorSM(Elevator elevator, FloorSubsystem floorSubsystem) {
 		this.elevator =  elevator;
 		this.floorSubsystem = floorSubsystem;
-		//wont need
-		ElevatorState[] statearr = {new DoorsClosed(this.elevator, this.floorSubsystem), new Moving(this.elevator, this.floorSubsystem), new Arrived(this.elevator), new DoorsOpen(this.elevator), new UpdateLamps(this.elevator), new End(this.elevator)};
+		this.current = ElevatorStates.DOORS_CLOSED;
 		generateTransitionHashmap();
 		generateStateHashmap();
-		states = statearr;
-		current = 0;
 	}
 	 
 	private void generateTransitionHashmap() {
-		this.transition = new HashMap<>();
-		transition.put(ElevatorStates.DOORS_CLOSED,  List.of(ElevatorStates.ARRIVED, ElevatorStates.MOVING, ElevatorStates.END));
-		transition.put(ElevatorStates.MOVING, List.of(ElevatorStates.ARRIVED));
-		transition.put(ElevatorStates.ARRIVED, List.of(ElevatorStates.DOORS_OPEN));
-		transition.put(ElevatorStates.DOORS_OPEN, List.of(ElevatorStates.UPDATE_LAMPS));
-		transition.put(ElevatorStates.UPDATE_LAMPS, List.of(ElevatorStates.DOORS_CLOSED));
+		this.transitions = new HashMap<>();
+		this.transitions.put(ElevatorStates.DOORS_CLOSED,  List.of(ElevatorStates.ARRIVED, ElevatorStates.MOVING, ElevatorStates.END));
+		this.transitions.put(ElevatorStates.MOVING, List.of(ElevatorStates.ARRIVED));
+		this.transitions.put(ElevatorStates.ARRIVED, List.of(ElevatorStates.DOORS_OPEN));
+		this.transitions.put(ElevatorStates.DOORS_OPEN, List.of(ElevatorStates.UPDATE_LAMPS));
+		this.transitions.put(ElevatorStates.UPDATE_LAMPS, List.of(ElevatorStates.DOORS_CLOSED));
 	}
 	
 	private void generateStateHashmap() {
-		this.s = new HashMap<>();
-		s.put(ElevatorStates.DOORS_CLOSED, new DoorsClosed(this.elevator, this.floorSubsystem));
-		s.put(ElevatorStates.MOVING,  new Moving(this.elevator, this.floorSubsystem));
-		s.put(ElevatorStates.ARRIVED,  new Arrived(this.elevator));
-		s.put(ElevatorStates.DOORS_OPEN, new DoorsOpen(this.elevator));
-		s.put(ElevatorStates.UPDATE_LAMPS, new UpdateLamps(this.elevator));
-		s.put(ElevatorStates.END, new End(this.elevator));
+		this.states = new HashMap<>();
+		this.states.put(ElevatorStates.DOORS_CLOSED, new DoorsClosed(this.elevator, this.floorSubsystem));
+		this.states.put(ElevatorStates.MOVING,  new Moving(this.elevator, this.floorSubsystem));
+		this.states.put(ElevatorStates.ARRIVED,  new Arrived(this.elevator));
+		this.states.put(ElevatorStates.DOORS_OPEN, new DoorsOpen(this.elevator));
+		this.states.put(ElevatorStates.UPDATE_LAMPS, new UpdateLamps(this.elevator));
+		this.states.put(ElevatorStates.END, new End(this.elevator));
 	}
 	
 	private void nextState(ElevatorStates nextState) {
-		if(transition.get(curr).contains(nextState))
-			curr = nextState; //else throw exception?
-		 //current = transitions[current][nextState];
+		if(transitions.get(current).contains(nextState))
+			current = nextState; //else throw exception?
     }
 	
 	public void activity(Direction direction) {
-		s.get(curr).activity(direction);
-		//states[current].activity(direction);
+		states.get(current).activity(direction);
 	}
 	
 	public void action(ArrayList<Integer> lamps) {
-		s.get(curr).action(lamps);
-		//states[current].action(lamps);
+		states.get(current).action(lamps);
 	}
 	
 	public void validRequest(Entry<Integer,Direction> destination) {
-		s.get(curr).validRequest(destination);
-		//states[current].validRequest(destination);
+		states.get(current).validRequest(destination);
 		nextState(ElevatorStates.MOVING);
 	}
 	public void invalidRequest() {
-		s.get(curr).invalidRequest();
-		//states[current].invalidRequest();
+		states.get(current).invalidRequest();
 		nextState(ElevatorStates.END);
 	}
 	
 	public void arrivesAtDestination() {
-		s.get(curr).arrivesAtDestination();
-		//states[current].arrivesAtDestination();
+		states.get(current).arrivesAtDestination();
 		nextState(ElevatorStates.ARRIVED);
 	}
 	
 	public void toggleDoors(ElevatorStates next) {
-		s.get(curr).toggleDoors();
-		//states[current].toggleDoors();
+		states.get(current).toggleDoors();
 		nextState(next);
 	}
 	
 	public ArrayList<Integer> getLamps() {
-		ArrayList<Integer> lamps = s.get(curr).getLamps();
-		//ArrayList<Integer> lamps = states[current].getLamps();
+		ArrayList<Integer> lamps = states.get(current).getLamps();
 		nextState(ElevatorStates.UPDATE_LAMPS);
 		return lamps;
 	}
 	
 	public void exit() {
-		s.get(curr).exit();
-		//states[current].exit();
-//		current = 6;
+		states.get(current).exit();
 	}
 	
 	
@@ -126,7 +109,7 @@ public class ElevatorSM implements Runnable{
 		Direction destinationDirection = Direction.STATIONARY;
 		ArrayList<Integer> lamps = null;
 		while (true) {
-			switch(curr) {
+			switch(current) {
 			case DOORS_CLOSED:
 				//Get the request of the next floor with the motor direction from the scheduler
 				Entry<Integer,Direction> destination = this.elevator.scheduler.getRequest(this.elevator.getElevatorLocation());
